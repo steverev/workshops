cd254d72c74516c528dbb714e59e62746b01cfc9
Steve Reveliotty <steverevel+git@gmail.com>
2018-04-12 15:59:40 -0400

    initial commit

--- 1_MongoDB/install.rb
+++ 1_MongoDB/install.rb
@@ -1,44 +1,23 @@
-##########################################################################
-# Cookbook Name:: mongodb
+#
+# Cookbook:: mongodb
 # Recipe:: install
 #
-# Not sure how to get started?
-#
-# You could:
-# 1.  copy the relevant commands from http://docs.mongodb.org/manual/tutorial/install-mongodb-on-red-hat-centos-or-fedora-linux/
-# 2.  comment out everything
-# 3.  add the Chef resources and other Chef code necessary
-#
-# This file is an example of steps 1 and 2 above.
-##########################################################################
-#
+# Copyright:: 2018, The Authors, All Rights Reserved.
 
-# Create a /etc/yum.repos.d/mongodb.repo file to hold the following configuration information for the MongoDB repository:
-#
-# If you are running a 64-bit system, use the following configuration:
-#
-# [mongodb]
-# name=MongoDB Repository
-# baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/
-# gpgcheck=0
-# enabled=1
-# If you are running a 32-bit system, which is not recommended for production deployments, use the following configuration:
-#
-# [mongodb]
-# name=MongoDB Repository
-# baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/i686/
-# gpgcheck=0
-# enabled=1
-#
-# Install the MongoDB packages and associated tools.
-#
-# sudo yum install mongodb-org
-#
-#
-# Start MongoDB.
-#
-# sudo service mongod start
-#
-# ensure that MongoDB will start following a system reboot by issuing the following command:
-#
-# sudo chkconfig mongod on#
+
+yum_repository 'mongodb' do
+  description 'MongoDB Repository'
+  baseurl "http://downloads-distro.mongodb.org/repo/redhat/os/x86_64"
+  gpgcheck false
+  enabled true
+  action :create
+end
+
+yum_package 'mongodb-org' do
+  action :install
+end
+
+service 'mongod' do
+  action [ :start, :enable ]
+  subscribes :reload, 'file[/etc/mongodb/mongo.conf]', :immediately
+end
